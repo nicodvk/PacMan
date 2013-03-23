18a4186a2cec1013d182b4f9b69d70ef1409d9b1
Nicolas Devynck <ndevynck@epsi.fr>
2013-03-23 22:43:04 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="fr-FR">
+	<head>
+		<title>PacMan</title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="start" title="Accueil" href="index.php"/>
+		<script type="text/javascript" src="js/jquery/jquery.js"></script>
+		<script type="text/javascript" src="js/js.js"></script>
+
+		<!--[if lt IE 9]>
+		<![endif]-->
+	</head>
+	<body>
+    <div class="container">
+        <canvas id="cnv" width="465" height="452">
+            Votre navigateur ne supporte pas le Canvas
+        </canvas>
+    </div>
+	</body>
+</html>
